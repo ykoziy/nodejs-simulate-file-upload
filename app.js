f43ecdcd8b370028499ca48fa61a609527ea1383
ykoziy <yukoziy@gmail.com>
2026-05-14 17:04:17 -0400

    test

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const client = new SecretsManagerClient({ region: "us-east-1" });
 const secret_name = "node-server/api-key";
 
 async function getApiKey() {
-  const response = await client.send(
-    new GetSecretValueCommand({ SecretId: secret_name })
-  );
-  const secret = JSON.parse(response.SecretString);
-  return secret.MY_API_KEY;
+    const response = await client.send(
+        new GetSecretValueCommand({ SecretId: secret_name })
+    );
+    const secret = JSON.parse(response.SecretString);
+    return secret.MY_API_KEY;
 }
 
 let VALID_API_KEY;
@@ -21,19 +21,20 @@ let VALID_API_KEY;
 getApiKey().then(key => { VALID_API_KEY = key; });
 
 const validateApiKey = (req, res, next) => {
-  const clientKey = req.header('api-key'); // Standard header for API keys
-  
-  if (!clientKey || clientKey !== VALID_API_KEY) {
-    return res.status(401).json({ error: "Forbidden: Invalid API Key" });
-  }
-  next();
+    const clientKey = req.header('api-key'); // Standard header for API keys
+    console.log("client key" + clientKey);
+    console.log("server api key" + VALID_API_KEY);
+    if (!clientKey || clientKey !== VALID_API_KEY) {
+        return res.status(401).json({ error: "Forbidden: Invalid API Key" });
+    }
+    next();
 };
 
 // Apply to your POST endpoint
 app.post('/uploadFile', validateApiKey, (req, res) => {
-  res.send("Authorized request successful!");
+    res.send("Authorized request successful!");
 });
 
 app.listen(3000, '0.0.0.0', () => {
-  console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
